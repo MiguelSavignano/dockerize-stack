a742a17ad062b53ab81fcfaaa80f5f00d4c0b3ce
Miguel Savignano <migue.masx@gmail.com>
2018-08-04 16:55:56 +0200

    refactor render templates method, for better test

--- lib/rails/dockerize_rails.rb
+++ lib/rails/dockerize_rails.rb
@@ -1,8 +1,8 @@
 require 'thor'
-
+# Ask variables and render templates
 class DockerizeRails < Thor
   include Thor::Actions
-  attr_accessor :ruby_version, :database
+  attr_accessor :ruby_version, :database, :id_rsa
 
   def self.source_root
     File.dirname(__FILE__)
@@ -10,29 +10,36 @@ class DockerizeRails < Thor
 
   desc 'generate_files', 'generate docker files for rails application'
   def generate_files
-    @ruby_version = ask("Ruby Version (default 2.4.3):")
-    @ruby_version = "2.4.3" if @ruby_version == ""
-    @database     = ask("What is your Database?", limited_to: ["mysql", "postgresql"])
-    @id_rsa       = ask("Would you need your id_rsa file to connect with GitHub? Type yes or no (default no):")
-    @id_rsa       = "no" if @id_rsa == ""
+    ruby_version = ask('Ruby Version (default 2.4.3):')
+    ruby_version = '2.4.3' if ruby_version == ''
+    database     = ask('What is your Database?', limited_to: ['postgresql', 'mysql'])
+    id_rsa       = ask('Would you need your id_rsa file to connect with GitHub? Type yes or no (default no):')
+    id_rsa       = 'no' if id_rsa == ''
+    render_templates(ruby_version: ruby_version, database: database, id_rsa: id_rsa)
+  end
 
-    template "templates/Dockerfile.erb", "docker/development/Dockerfile"
-    template "templates/entrypoint.sh.erb", "docker/development/entrypoint.sh"
-    template "templates/docker-compose.yml.erb", "docker-compose.yml"
-    template "templates/database-docker.yml.erb", "config/database-docker.yml"
-    template "templates/dockerignore.erb", ".dockerignore"
+  def render_templates(ruby_version: '2.4.3', database: 'postgresql', id_rsa: 'no')
+    @ruby_version = ruby_version
+    @database     = database
+    @id_rsa       = id_rsa
 
-    append_to_file '.gitignore', "
-volumes"
+    template 'templates/Dockerfile.erb', 'docker/development/Dockerfile'
+    template 'templates/entrypoint.sh.erb', 'docker/development/entrypoint.sh'
+    template 'templates/docker-compose.yml.erb', 'docker-compose.yml'
+    template 'templates/database-docker.yml.erb', 'config/database-docker.yml'
+    template 'templates/dockerignore.erb', '.dockerignore'
+
+    append_to_file '.gitignore', '
+volumes'
     if @id_rsa == 'yes'
-      template "templates/id_rsa.sample", "docker/development/id_rsa.sample"      
-      append_to_file '.gitignore', "
-docker/development/id_rsa"
-      append_to_file '.gitignore', "
-docker/development/id_rsa.sample"
+      template 'templates/id_rsa.sample', 'docker/development/id_rsa.sample'
+      append_to_file '.gitignore', '
+docker/development/id_rsa'
+      append_to_file '.gitignore', '
+docker/development/id_rsa.sample'
     end
-    
-    puts "Update your database.yml based in database-docker.yml"
+
+    puts 'Update your database.yml based in database-docker.yml'
   end
 
 end
