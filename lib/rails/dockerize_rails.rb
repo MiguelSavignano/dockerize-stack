4df545fe005197fd6ac26a695b2fdd04f0ec99ef
Miguel Savignano <migue.masx@gmail.com>
2018-08-05 07:14:03 +0200

    disable render_templates as a command

--- lib/rails/dockerize_rails.rb
+++ lib/rails/dockerize_rails.rb
@@ -4,6 +4,7 @@ class DockerizeRails < Thor
   include Thor::Actions
   attr_accessor :ruby_version, :database, :id_rsa
 
+
   def self.source_root
     File.dirname(__FILE__)
   end
@@ -18,28 +19,28 @@ class DockerizeRails < Thor
     render_templates(ruby_version: ruby_version, database: database, id_rsa: id_rsa)
   end
 
-  def render_templates(ruby_version: '2.5.1-slim', database: 'postgresql', id_rsa: 'no')
-    @ruby_version = ruby_version
-    @database     = database
-    @id_rsa       = id_rsa
+  no_commands do
+    def render_templates(ruby_version: '2.5.1-slim', database: 'postgresql', id_rsa: 'no')
+      @ruby_version = ruby_version
+      @database     = database
+      @id_rsa       = id_rsa
 
-    template 'templates/docker/development/Dockerfile.erb', 'docker/development/Dockerfile'
-    template 'templates/docker/development/entrypoint.sh.erb', 'docker/development/entrypoint.sh'
-    template 'templates/docker-compose.yml.erb', 'docker-compose.yml'
-    template 'templates/config/database-docker.yml.erb', 'config/database-docker.yml'
-    template 'templates/dockerignore.erb', '.dockerignore'
+      template 'templates/docker/development/Dockerfile.erb', 'docker/development/Dockerfile'
+      template 'templates/docker/development/entrypoint.sh.erb', 'docker/development/entrypoint.sh'
+      template 'templates/docker-compose.yml.erb', 'docker-compose.yml'
+      template 'templates/config/database-docker.yml.erb', 'config/database-docker.yml'
+      template 'templates/dockerignore.erb', '.dockerignore'
 
-    append_to_file '.gitignore', '
+      append_to_file '.gitignore', '
 volumes'
-    if @id_rsa == 'yes'
-      template 'templates/id_rsa.sample', 'id_rsa.sample'
-      append_to_file '.gitignore', '
+      if @id_rsa == 'yes'
+        template 'templates/id_rsa.sample', 'id_rsa.sample'
+        append_to_file '.gitignore', '
 id_rsa'
-      append_to_file '.gitignore', '
+        append_to_file '.gitignore', '
 id_rsa.sample'
+      end
+      puts 'Update your database.yml based in database-docker.yml'
     end
-
-    puts 'Update your database.yml based in database-docker.yml'
   end
-
 end
