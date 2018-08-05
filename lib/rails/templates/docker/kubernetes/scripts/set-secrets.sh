9dbb93446434a7edb4ac324cd2a441123fe82e95
Miguel Savignano <migue.masx@gmail.com>
2018-08-05 18:28:10 +0200

    set rails mater key

--- lib/rails/templates/docker/kubernetes/scripts/set-secrets.sh
+++ lib/rails/templates/docker/kubernetes/scripts/set-secrets.sh
@@ -2,4 +2,4 @@ kubectl create secret generic rails-secrets \
   --from-literal=DATABASE_USERNAME= \
   --from-literal=DATABASE_PASSWORD= \
   --from-literal=SECRET_KEY_BASE= \
-  --from-literal=RAILS_MASTERKEY=
+  --from-literal=RAILS_MASTER_KEY=
