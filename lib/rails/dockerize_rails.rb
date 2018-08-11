ba5c612ed10bec3252fb6ebb69c3452a77bc8df3
Miguel Savignano <migue.masx@gmail.com>
2018-08-11 03:19:55 +0200

    generate files in other folder

--- lib/rails/dockerize_rails.rb
+++ lib/rails/dockerize_rails.rb
@@ -4,6 +4,7 @@ class DockerizeRails < Thor
   include Thor::Actions
   attr_accessor :ruby_version, :database, :id_rsa
 
+  WORKDIR = "./examples/rails".freeze
 
   def self.source_root
     File.dirname(__FILE__)
@@ -28,19 +29,19 @@ class DockerizeRails < Thor
       @database     = database
       @id_rsa       = id_rsa
 
-      template 'templates/docker/development/Dockerfile.erb', 'docker/development/Dockerfile'
-      template 'templates/docker/development/entrypoint.sh.erb', 'docker/development/entrypoint.sh'
-      template 'templates/docker-compose.yml.erb', 'docker-compose.yml'
-      template 'templates/config/database-docker.yml.erb', 'config/database-docker.yml'
-      template 'templates/dockerignore.erb', '.dockerignore'
+      template 'templates/docker/development/Dockerfile.erb', "#{WORKDIR}/docker/development/Dockerfile"
+      template 'templates/docker/development/entrypoint.sh.erb', "#{WORKDIR}/docker/development/entrypoint.sh"
+      template 'templates/docker-compose.yml.erb', "#{WORKDIR}/docker-compose.yml"
+      template 'templates/config/database-docker.yml.erb', "#{WORKDIR}/config/database-docker.yml"
+      template 'templates/dockerignore.erb', "#{WORKDIR}/.dockerignore"
 
-      append_to_file '.gitignore', '
+      append_to_file "#{WORKDIR}/.gitignore", '
 volumes'
       if @id_rsa == 'yes'
-        template 'templates/id_rsa.sample', 'id_rsa.sample'
-        append_to_file '.gitignore', '
+        template 'templates/id_rsa.sample', "#{WORKDIR}/id_rsa.sample"
+        append_to_file "#{WORKDIR}/.gitignore", '
 id_rsa'
-        append_to_file '.gitignore', '
+        append_to_file "#{WORKDIR}/.gitignore", '
 id_rsa.sample'
       end
       puts 'Update your database.yml based in database-docker.yml'
@@ -51,10 +52,10 @@ id_rsa.sample'
       @database     = database
       @id_rsa       = id_rsa
 
-      directory 'templates/docker/production', 'docker/production'
-      directory 'templates/docker/kubernetes', 'docker/kubernetes'
+      directory 'templates/docker/production', "#{WORKDIR}/docker/production"
+      directory 'templates/docker/kubernetes', "#{WORKDIR}docker/kubernetes"
 
-      template 'templates/docker/Dockerfile.production.erb', 'docker/production/rails/Dockerfile'
+      template 'templates/docker/Dockerfile.production.erb', "#{WORKDIR}docker/production/rails/Dockerfile"
     end
   end
 end
