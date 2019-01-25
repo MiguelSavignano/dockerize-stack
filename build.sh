d1e9b8c992f779159ed86b28317218d10ad8951c
Miguel Savignano <migue.masx@gmail.com>
2019-01-25 10:47:28 +0100

    upgrade version

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 gem uninstall -a -x dockerize-stack
 gem build dockerize_stack.gemspec
-gem install dockerize-stack-0.2.0.gem
+gem install dockerize-stack-0.3.0.gem
--- lib/version.rb
+++ lib/version.rb
@@ -1,3 +1,3 @@
 module DockerizeStack
-  VERSION = "0.2.0"
+  VERSION = "0.3.0"
 end
