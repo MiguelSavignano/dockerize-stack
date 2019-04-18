c0f94e2bf0218c7e305d6bee4adf55bd9c878a79
Miguel Savignano <migue.masx@gmail.com>
2019-04-18 20:43:04 +0200

    create react app

---
+++ lib/react/dockerize_react.rb
@@ -0,0 +1,41 @@
+require 'thor'
+# Ask variables and render templates
+class DockerizeReact < Thor
+  include Thor::Actions
+  attr_accessor :nodejs_version
+
+  WORKDIR = ".".freeze
+
+  def self.source_root
+    File.dirname(__FILE__)
+  end
+
+  desc 'generate_files', 'generate docker files for react create app'
+  def generate_files
+    @nodejs_version = ask_with_default('Nodejs Version (default 10):', '10')
+
+    render_templates
+  end
+
+  no_commands do
+    def render_templates
+      template 'templates/docker/Dockerfile.erb', "#{WORKDIR}/docker/Dockerfile"
+      directory 'templates/docker/nginx', "#{WORKDIR}/docker/nginx"
+    end
+  end
+
+  private
+
+  def ask_with_default(question = '', default = '')
+    result = ask(question)
+    result != '' ? result : default
+  end
+
+  def append_or_create(file_path, file_content)
+    if File.exist?(file_path)
+      append_to_file file_path, file_content
+    else
+      create_file file_path, file_content
+    end
+  end
+end
