843c9fc5cf5095d9fbd038f176043d1d706bf11d
Miguel Savignano <miguel.savignano@the-cocktail.com>
2019-09-05 17:09:44 +0200

    react

--- lib/dockerize_stack/dockerize_react.rb
+++ lib/dockerize_stack/dockerize_react.rb
@@ -1,42 +1,42 @@
 require 'thor'
 # Ask variables and render templates
-class DockerizeReact < Thor
-  include Thor::Actions
-  attr_accessor :nodejs_version
+module DockerizeStack
+  # Ask variables and render templates
+  class DockerizeReact < ThorExtend
+    attr_accessor :nodejs_version
 
-  WORKDIR = ".".freeze
-
-  def self.source_root
-    "#{File.dirname(__FILE__)}/../../templates"
-  end
-
-  desc 'generate_files', 'generate docker files for react create app'
-  def generate_files
-    @nodejs_version = ask_with_default('Nodejs Version (default 10):', '10')
-
-    render_templates
-  end
-
-  no_commands do
-    def render_templates
-      template 'react/docker/Dockerfile.erb', "#{WORKDIR}/docker/Dockerfile"
-      template 'react/dockerignore.erb', "#{WORKDIR}/.dockerignore"
-      directory 'react/docker/nginx', "#{WORKDIR}/docker/nginx"
+    def self.source_root
+      "#{File.dirname(__FILE__)}/../../templates"
     end
-  end
 
-  private
+    desc 'generate_files', 'generate docker files for react create app'
+    def generate_files
+      @nodejs_version = ask_with_default('Nodejs Version (default 10):', '10')
 
-  def ask_with_default(question = '', default = '')
-    result = ask(question)
-    result != '' ? result : default
-  end
+      render_templates
+    end
 
-  def append_or_create(file_path, file_content)
-    if File.exist?(file_path)
-      append_to_file file_path, file_content
-    else
-      create_file file_path, file_content
+    no_commands do
+      def render_templates
+        template 'react/docker/Dockerfile.erb', "#{WORKDIR}/docker/Dockerfile"
+        template 'react/dockerignore.erb', "#{WORKDIR}/.dockerignore"
+        directory 'react/docker/nginx', "#{WORKDIR}/docker/nginx"
+      end
+    end
+
+    private
+
+    def ask_with_default(question = '', default = '')
+      result = ask(question)
+      result != '' ? result : default
+    end
+
+    def append_or_create(file_path, file_content)
+      if File.exist?(file_path)
+        append_to_file file_path, file_content
+      else
+        create_file file_path, file_content
+      end
     end
   end
 end
