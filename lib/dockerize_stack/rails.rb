337cf70fad3dbc719c6a372eda2af3dfb41c9ecf
Miguel Savignano <miguel.savignano@the-cocktail.com>
2019-09-26 02:22:25 +0200

    Rails javasscript package manager (#24)
    * generate Dockerfile with npm, yarn and assets_pipeline

--- lib/dockerize_stack/rails.rb
+++ lib/dockerize_stack/rails.rb
@@ -4,25 +4,28 @@ module DockerizeStack
     include Thor::Actions
     include ThorActionsExtend
 
-    attr_accessor :ruby_version, :nodejs_version, :yarn_version,
-      :database, :github_private, :kubernetes, :workdir
+    attr_accessor :ruby_version, :javascrit_package_manager, :nodejs_version,
+                  :yarn_version, :database, :github_private, :kubernetes, :workdir
 
     def self.source_root
       "#{File.dirname(__FILE__)}/../../templates/rails"
     end
 
     no_commands do
-      def generate_files(path:)
+      def fetch_template_variables(path:)
         @workdir = path || '.'
         @nodejs_version = '10.16.3'
         @yarn_version = '1.17.3'
-
         @ruby_version = ask_with_default('Ruby Version (default 2.5.6):', '2.5.6')
+        @javascrit_package_manager = ask('What is your Javascript package manager?', limited_to: ['yarn', 'npm', 'asset_pipeline'])
         @database = ask('What is your Database?', limited_to: ['postgresql', 'mysql'])
         @rails_worker = ask_with_default('You need worker service (sidekiq example) (default yes):', 'yes')
         @github_private = ask_with_default('You need github token for private gems? (default no):', 'no')
         @kubernetes = ask_with_default('You want generate docker-stack for kubernetes? y/n', 'n')
+      end
 
+      def generate_files(path:)
+        fetch_template_variables(path: path)
         render_templates
         render_kubernetes_templates unless ['n', 'no', ''].include?(@kubernetes)
       end
