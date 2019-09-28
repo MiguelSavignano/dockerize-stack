23637a228a4b605caa88b3b05fb4fd7759c8cd87
Miguel Savignano <migue.masx@gmail.com>
2019-09-29 00:34:31 +0200

    Cli options (#25)
    * templates variables edit in cli arguents
    
    * add descriptions for cli options
    
    * refactor test to cli options
    
    * rename generate_file method
    
    * refactor run method
    
    * fix ask boolean
    
    * update readme

--- lib/dockerize_stack/rails.rb
+++ lib/dockerize_stack/rails.rb
@@ -5,29 +5,29 @@ module DockerizeStack
     include ThorActionsExtend
 
     attr_accessor :ruby_version, :javascrit_package_manager, :nodejs_version,
-                  :yarn_version, :database, :github_private, :kubernetes, :workdir
-
-    def self.source_root
-      "#{File.dirname(__FILE__)}/../../templates/rails"
-    end
+                  :yarn_version, :database, :github_private, :kubernetes, :output_folder
 
     no_commands do
-      def fetch_template_variables(path:)
-        @workdir = path || '.'
-        @nodejs_version = '10.16.3'
-        @yarn_version = '1.17.3'
-        @ruby_version = ask_with_default('Ruby Version (default 2.5.6):', '2.5.6')
-        @javascrit_package_manager = ask('What is your Javascript package manager?', limited_to: ['yarn', 'npm', 'asset_pipeline'])
-        @database = ask('What is your Database?', limited_to: ['postgresql', 'mysql'])
-        @rails_worker = ask_with_default('You need worker service (sidekiq example) (default yes):', 'yes')
-        @github_private = ask_with_default('You need github token for private gems? (default no):', 'no')
-        @kubernetes = ask_with_default('You want generate docker-stack for kubernetes? y/n', 'n')
+      def template_type
+        :rails
       end
 
-      def generate_files(path:)
-        fetch_template_variables(path: path)
-        render_templates
-        render_kubernetes_templates unless ['n', 'no', ''].include?(@kubernetes)
+      def run(options)
+        super(options)
+        render_kubernetes_templates if @kubernetes
+      end
+
+      def fetch_template_variables
+        @output_folder             = with_default(:output_folder)
+        @nodejs_version            = with_default(:nodejs_version)
+        @yarn_version              = with_default(:yarn_version)
+
+        @ruby_version              = ask_with_default(:ruby_version)
+        @javascrit_package_manager = ask_with_options(:javascrit_package_manager)
+        @database                  = ask_with_options(:database)
+        @rails_worker              = ask_with_default_boolean(:rails_worker)
+        @github_private            = ask_with_default_boolean(:github_private)
+        @kubernetes                = ask_with_default_boolean(:kubernetes)
       end
 
       def render_templates
@@ -41,7 +41,7 @@ module DockerizeStack
       end
 
       def render_kubernetes_templates
-        directory 'kubernetes', "#{@workdir}/kubernetes"
+        directory 'kubernetes', "#{@output_folder}/kubernetes"
       end
     end
   end
