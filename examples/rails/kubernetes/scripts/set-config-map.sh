9bba0a03e2e810b617f283c15312e50928198591
Miguel Savignano <migue.masx@gmail.com>
2020-02-08 01:56:55 +0100

    render kubernetes folder

--- examples/rails/kubernetes/scripts/set-config-map.sh
+++
@@ -1,4 +0,0 @@
-kubectl create configmap rails-config \
-  --from-literal=DATABASE_HOST=cloudsql-proxy \
-  --from-literal=DATABASE_NAME=rails_production \
-  --from-literal=RAILS_LOG_TO_STDOUT=true
